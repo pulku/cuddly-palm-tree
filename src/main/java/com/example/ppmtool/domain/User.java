fd1ba08f4d23af52e3efb467ee0d40dd26a204b7
pinarulku <pinarul@hotmail.com>
2019-02-12 00:06:38 +0300

    removed user details

--- src/main/java/com/example/ppmtool/domain/User.java
+++ src/main/java/com/example/ppmtool/domain/User.java
@@ -1,7 +1,7 @@
 package com.example.ppmtool.domain;
 
-import com.example.ppmtool.security.UserDetails;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
@@ -10,7 +10,7 @@ import java.util.Collection;
 import java.util.Date;
 
 @Entity
-public class User extends UserDetails {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -60,6 +60,26 @@ public class User extends UserDetails {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 
     public void setUsername(String username) {
         this.username = username;
@@ -73,6 +93,11 @@ public class User extends UserDetails {
         this.fullName = fullName;
     }
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 
     public String getPassword() {
         return password;
