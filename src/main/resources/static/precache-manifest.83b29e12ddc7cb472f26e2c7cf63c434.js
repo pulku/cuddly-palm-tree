35259f59883ada94de2390b55dfbd8733778bbf7
pinarulku <pinarul@hotmail.com>
2019-02-17 17:09:15 +0300

    react fe

---
+++ src/main/resources/static/precache-manifest.83b29e12ddc7cb472f26e2c7cf63c434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36aefb33d05ad6d17451",
+    "url": "/static/js/main.36aefb33.chunk.js"
+  },
+  {
+    "revision": "6248c8025371b67ed96e",
+    "url": "/static/js/1.6248c802.chunk.js"
+  },
+  {
+    "revision": "36aefb33d05ad6d17451",
+    "url": "/static/css/main.6fd87b1f.chunk.css"
+  },
+  {
+    "revision": "6248c8025371b67ed96e",
+    "url": "/static/css/1.2db6ab26.chunk.css"
+  },
+  {
+    "revision": "f4bae58b567a0b8ccf5429a89217896e",
+    "url": "/index.html"
+  }
+];
